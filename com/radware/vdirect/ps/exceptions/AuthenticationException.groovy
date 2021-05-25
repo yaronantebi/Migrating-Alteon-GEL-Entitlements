b7a20bc4e3b7446f5d20058bda80e9b299929e6c
yarona <yarona@radware.com>
2021-05-25 14:52:40 +0300

    v1

---
+++ com/radware/vdirect/ps/exceptions/AuthenticationException.groovy
@@ -0,0 +1,7 @@
+package com.radware.vdirect.ps.exceptions
+
+class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message)
+    }
+}
---
+++ com/radware/vdirect/ps/exceptions/NotFoundException.groovy
@@ -0,0 +1,7 @@
+package com.radware.vdirect.ps.exceptions
+
+class NotFoundException extends RuntimeException {
+    NotFoundException(String msg) {
+        super(msg)
+    }
+}
